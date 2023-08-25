2e36d2d6f774016f958ec1b6aff6ceaa8b22dfe8
ecdevguy <123844520+ecdevguy@users.noreply.github.com>
2023-08-26 00:54:02 +0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -4,32 +4,52 @@ let errors = 0;
 
 $(document).ready(() => {
   
-
-  $(".pinyin-textbox").on("submit", function(event){
-    event.preventDefault();
-    if (($("#chinese-character").attr("pinyin") == $("#pinyin-character").val())) {
-        
-      let x = Math.floor((Math.random() * 135));
-      $("#chinese-character").text(myJson[x].Word);
-      $("#chinese-character").attr("pinyin", myJson[x].OtherPinyin)
-      $("#chinese-character").attr("character", myJson[x].Word)
-      $("#pinyin-character").val("");
-      $("#pinyin-character").attr("placeholder", "");
+  $(".pinyin-textbox").on("submit", function(e){
+    e.preventDefault();
+    if (e.which === 13 && ($("#chinese-character").attr("pinyin") == $("#pinyin-character").val())) {
       
-      $("#body").attr("class", "correct");
-      $("#definition").text(myJson[x]['First Translation']);
-      let errors = 0;
-  }
-  else if (($("#chinese-character").attr("character") != $("#pinyin-character").val()) && errors < 7) {
+    let x = Math.floor((Math.random() * 135));
+    $("#chinese-character").text(myJson[x].Word);
+    $("#chinese-character").attr("pinyin", myJson[x].OtherPinyin)
+    $("#chinese-character").attr("character", myJson[x].Word)
     $("#pinyin-character").val("");
-    $("#body").attr("class", "incorrect");
+    $("#pinyin-character").attr("placeholder", "");
     
-    errors++;
-  }
-  else if (errors == 7){
+    $("#body").attr("class", "correct");
+    $("#definition").text(myJson[x]['First Translation']);
+    let errors = 0;
+}
+else if (e.which === 13 && ($("#chinese-character").attr("character") != $("#pinyin-character").val()) && errors < 7) {
+  $("#pinyin-character").val("");
+  $("#body").attr("class", "incorrect");
+  
+  errors++;
+}
+else if (e.which === 36 && ($("#chinese-character").attr("pinyin") == $("#pinyin-character").val())) {
+      
+    let x = Math.floor((Math.random() * 135));
+    $("#chinese-character").text(myJson[x].Word);
+    $("#chinese-character").attr("pinyin", myJson[x].OtherPinyin)
+    $("#chinese-character").attr("character", myJson[x].Word)
     $("#pinyin-character").val("");
-    $("#pinyin-character").attr("placeholder", $("#chinese-character").attr("pinyin"));
-    errors = 4;
-  }
+    $("#pinyin-character").attr("placeholder", "");
+    
+    $("#body").attr("class", "correct");
+    $("#definition").text(myJson[x]['First Translation']);
+    let errors = 0;
+}
+else if (e.which === 36 && ($("#chinese-character").attr("character") != $("#pinyin-character").val()) && errors < 7) {
+  $("#pinyin-character").val("");
+  $("#body").attr("class", "incorrect");
+  
+  errors++;
+}
+else if (errors == 7){
+  $("#pinyin-character").val("");
+  $("#pinyin-character").attr("placeholder", $("#chinese-character").attr("pinyin"));
+  errors = 4;
+}
   });
 });
+
+
