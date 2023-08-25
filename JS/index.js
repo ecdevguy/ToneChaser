a732bc7bf06fd01f6f7ccc2a4b6a595773e784f1
ecdevguy <123844520+ecdevguy@users.noreply.github.com>
2023-08-25 22:49:07 +0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -29,8 +29,28 @@ else if (e.which === 13 && ($("#chinese-character").attr("character") != $("#pin
   
   errors++;
 }
+else if (e.which === 36 && ($("#chinese-character").attr("pinyin") == $("#pinyin-character").val())) {
+      
+    let x = Math.floor((Math.random() * 135));
+    $("#chinese-character").text(myJson[x].Word);
+    $("#chinese-character").attr("pinyin", myJson[x].OtherPinyin)
+    $("#chinese-character").attr("character", myJson[x].Word)
+    $("#pinyin-character").val("");
+    $("#pinyin-character").attr("placeholder", "");
+    
+    $("#body").attr("class", "correct");
+    $("#definition").text(myJson[x]['First Translation']);
+    let errors = 0;
+}
+else if (e.which === 36 && ($("#chinese-character").attr("character") != $("#pinyin-character").val()) && errors < 7) {
+  $("#pinyin-character").val("");
+  $("#body").attr("class", "incorrect");
+  
+  errors++;
+}
 else if (errors == 7){
   $("#pinyin-character").val("");
   $("#pinyin-character").attr("placeholder", $("#chinese-character").attr("pinyin"));
   errors = 4;
-}})
+}
+})
